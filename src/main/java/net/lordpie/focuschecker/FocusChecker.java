cba9d9a041203d1b4644d79faf87ecc137afdb2a
LordPie <contact@lordpie.net>
2019-04-16 06:35:07 +0100

    Change to versioning.

--- src/main/java/net/lordpie/focuschecker/FocusChecker.java
+++ src/main/java/net/lordpie/focuschecker/FocusChecker.java
@@ -24,7 +24,7 @@ import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 public class FocusChecker {
     public static final String MODID = "focuschecker";
     public static final String NAME = "Focus Checker";
-    public static final String VERSION = "0.1";
+    public static final String VERSION = "0.0.2";
 
     @Mod.EventHandler
     public void postInit(FMLPostInitializationEvent e) {
