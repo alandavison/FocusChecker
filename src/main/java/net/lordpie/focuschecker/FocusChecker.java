96db88464b5c17a36b7d359cf9ecb8da8c0c1cd4
LordPie <contact@lordpie.net>
2019-04-17 04:09:07 +0100

    Additional commenting.

--- src/main/java/net/lordpie/focuschecker/FocusChecker.java
+++ src/main/java/net/lordpie/focuschecker/FocusChecker.java
@@ -22,12 +22,12 @@ import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 
 @Mod(modid = FocusChecker.MODID, name = FocusChecker.NAME, version = FocusChecker.VERSION)
 public class FocusChecker {
-    public static final String MODID = "focuschecker";
-    public static final String NAME = "Focus Checker";
-    public static final String VERSION = "0.0.2";
+    public static final String MODID = "focuschecker"; //
+    public static final String NAME = "Focus Checker"; // Forge @Mod information.
+    public static final String VERSION = "0.0.2";      //
 
     @Mod.EventHandler
-    public void postInit(FMLPostInitializationEvent e) {
-        MinecraftForge.EVENT_BUS.register(new GUIHandler());
+    public void postInit(FMLPostInitializationEvent e) { // Registered to be called post-forge-initialisation.
+        MinecraftForge.EVENT_BUS.register(new GUIHandler()); // Register GUI update handler function.
     }
 }
